cc135f148c1c132d2fc81ac8a673fa97b451bab1
Elena Kappler (Laptop) <Elena.Kappler.1003@student.uu.se>
2026-01-01 16:07:17 +0100

    Refactor: Move Background/Star to separate files
    Extract Star and Background structs and their methods from game.cpp into new Background.h and Background.cpp files. Update project files to include the new source/header. Improves code organization and maintainability.

---
+++ Source/Background.cpp
@@ -0,0 +1,58 @@
+#pragma once
+#include "raylib_wrapper.h"
+#include "Background.h"
+
+
+// TODO: write constructor for Star?
+//BACKGROUND
+void Star::Update(float starOffset)
+{
+	position.x = initPosition.x + starOffset;
+	position.y = initPosition.y;
+
+}
+
+void Star::Render()
+{
+	// TODO: fix C-style casts
+	DrawCircle((int)position.x, (int)position.y, size, color);
+}
+
+// TODO: write constructor for Background
+void Background::Initialize(int starAmount)
+{
+	for (int i = 0; i < starAmount; i++)
+	{
+		// TODO: remove multi-step initialization
+		Star newStar;
+
+		newStar.initPosition.x = GetRandomValue(-150, GetScreenWidth() + 150);
+		newStar.initPosition.y = GetRandomValue(0, GetScreenHeight());
+
+		//random color?
+		newStar.color = SKYBLUE;
+
+		newStar.size = GetRandomValue(1, 4) / 2;
+
+		Stars.push_back(newStar);
+
+	}
+}
+
+// TODO: use ranged for-loops
+void Background::Update(float offset)
+{
+	for (int i = 0; i < Stars.size(); i++)
+	{
+		Stars[i].Update(offset);
+	}
+
+}
+
+void Background::Render()
+{
+	for (int i = 0; i < Stars.size(); i++)
+	{
+		Stars[i].Render();
+	}
+}
--- Source/game.cpp
+++ Source/game.cpp
@@ -1,4 +1,5 @@
 #include "game.h"
+#include "Background.h"
 #include <iostream>
 #include <vector>
 #include <chrono>
@@ -817,59 +818,4 @@ void Alien::Render(Texture2D texture)
 		}, {50 , 50},
 		0,
 		WHITE);
-}
-
-
-// TODO: write constructor for Star?
-//BACKGROUND
-void Star::Update(float starOffset)
-{
-	position.x = initPosition.x + starOffset;
-	position.y = initPosition.y;
-
-}
-
-void Star::Render()
-{
-	// TODO: fix C-style casts
-	DrawCircle((int)position.x, (int)position.y, size, color);
-}
-
-// TODO: write constructor for Background
-void Background::Initialize(int starAmount)
-{
-	for (int i = 0; i < starAmount; i++)
-	{
-		// TODO: remove multi-step initialization
-		Star newStar;
-
-		newStar.initPosition.x = GetRandomValue(-150, GetScreenWidth() + 150);
-		newStar.initPosition.y = GetRandomValue(0, GetScreenHeight());
-		
-		//random color?
-		newStar.color = SKYBLUE;
-
-		newStar.size = GetRandomValue(1, 4) / 2;
-
-		Stars.push_back(newStar);
-
-	}
-}
-
-// TODO: use ranged for-loops
-void Background::Update(float offset)
-{
-	for (int i = 0; i < Stars.size(); i++)
-	{
-		Stars[i].Update(offset);
-	}
-	
-}
-
-void Background::Render()
-{
-	for (int i = 0; i < Stars.size(); i++)
-	{
-		Stars[i].Render();
-	}
 }
