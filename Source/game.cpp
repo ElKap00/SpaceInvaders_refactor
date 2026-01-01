0a29252bbd7161a678e0a0e3eed6c57018593725
Elena Kappler (Laptop) <Elena.Kappler.1003@student.uu.se>
2026-01-01 16:29:44 +0100

    Refactor Projectile into separate source/header files
    Moved Projectile class from game.h to new Projectile.h/cpp files. Updated project files to include Projectile.cpp and Projectile.h. Adjusted includes to ensure Projectile is accessible where needed. Implementation of Update and Render methods moved from game.cpp to Projectile.cpp.

--- Source/game.cpp
+++ Source/game.cpp
@@ -630,42 +630,6 @@ bool Game::CheckCollision(Vector2 circlePos, float circleRadius, Vector2 lineSta
 
 }
 
-void Projectile::Update()
-{
-	position.y -= speed;
-
-	// UPDATE LINE POSITION
-	lineStart.y = position.y - 15;
-	lineEnd.y   = position.y + 15;
-
-	lineStart.x = position.x;
-	lineEnd.x   = position.x;
-
-	if (position.y < 0 || position.y > 1500)
-	{
-		active = false;
-	}
-}
-
-void Projectile::Render(Texture2D texture)
-{
-	DrawTexturePro(texture,
-		{
-			0,
-			0,
-			176,
-			176,
-		},
-		{
-			position.x,
-			position.y,
-			50,
-			50,
-		}, { 25 , 25 },
-		0,
-		WHITE);
-}
-
 void Wall::Render(Texture2D texture)
 {
 	DrawTexturePro(texture,
--- Source/game.h
+++ Source/game.h
@@ -7,6 +7,7 @@
 #include "Background.h"
 #include "EntityType.h"
 #include "Alien.h"
+#include "Projectile.h"
 
 
 enum struct State
@@ -20,24 +21,6 @@ struct PlayerData
 {
 	std::string name;
 	int score;
-};
-
-struct Projectile
-{
-public: 
-	// INITIALIZE PROJECTILE WHILE DEFINING IF ITS PLAYER OR ENEMY 
-	Vector2 position = {0,0};
-	int speed = 15; 
-	bool active = true;  
-	EntityType type = {};
-
-	// LINE WILL UPDATE WITH POSITION FOR CALCULATIONS
-	Vector2 lineStart = { 0, 0 };
-	Vector2 lineEnd = { 0, 0 };
-
-	void Update();
-
-	void Render(Texture2D texture);
 };
 
 struct Wall 
